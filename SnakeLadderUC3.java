49c705c33105bb87b2adc5f06c9ae2a279bb41be
batista12 <50904472+batista12@users.noreply.github.com>
2020-09-25 16:44:00 -0700

    Update SnakeLadderUC3.java

--- SnakeLadderUC3.java
+++ SnakeLadderUC3.java
@@ -1,21 +1,22 @@
 public class SnakeLadderUC3 {
    public static void main(String[] args){
-   
+
       int rollDice =(int)(Math.random() * 6) +1;
-      int PLAYER_POSITION=0;
-      int option =(int)(Math.random() * 3);
+      int PLAYER_POSITION=0,option=0;
+      option =(int)(Math.random() * 3);
       switch(option){
-      case LADDER:
+      case 1:
                   PLAYER_POSITION+=rollDice;
                   System.out.println("Ladder " + PLAYER_POSITION);
                   break;
-      case SNAKE:
+      case 2:
                   PLAYER_POSITION-=rollDice;
                   System.out.println("Snake " + PLAYER_POSITION);
                   break;
        default:
-                  System.out.println("No Play" + PLAYER_POSITION);
-      
+                  System.out.println("No Play " + PLAYER_POSITION);
+
    }
 }
+      
 }
