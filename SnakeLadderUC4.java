a962de11b533e479fe5c8a9f6a8039b331d8859c
batista12 <50904472+batista12@users.noreply.github.com>
2020-09-25 16:55:24 -0700

    Create SnakeLadderUC4.java

---
+++ SnakeLadderUC4.java
@@ -0,0 +1,25 @@
+public class SnakeLadderUC4 {
+   public static void main(String[] args){
+d0{
+      int rollDice =(int)(Math.random() * 6) +1;
+      int PLAYER_POSITION=0,option=0;
+      option =(int)(Math.random() * 3);
+      switch(option){
+      case 1:
+                  PLAYER_POSITION+=rollDice;
+                  System.out.println("Ladder " + PLAYER_POSITION);
+                  break;
+      case 2:
+                  PLAYER_POSITION-=rollDice;
+                  System.out.println("Snake " + PLAYER_POSITION);
+                  break;
+       default:
+                  System.out.println("No Play " + PLAYER_POSITION);
+
+   }
+   if(PLAYER_POSITION<0)
+   PLAYER_POSITION=0;
+   }while(POSITION_PLAYER == 100);
+   }
+}
+      
