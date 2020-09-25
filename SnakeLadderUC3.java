bae65976020a45240f1793b95fdc64ff46738486
batista12 <50904472+batista12@users.noreply.github.com>
2020-09-25 16:29:48 -0700

    Create SnakeLadderUC3.java

---
+++ SnakeLadderUC3.java
@@ -0,0 +1,20 @@
+public class SnakeLadderUC2 {
+   public static void main(String[] args){
+   
+      int rollDice =(int)(Math.random() * 6) +1;
+      int PLAYER_POSITION=0;
+      int option =(int)(Math.random() * 3);
+      switch(option){
+      case LADDER:
+                  PLAYER_POSITION+=rollDice;
+                  System.out.println("Ladder " + PLAYER_POSITION);
+                  break;
+      case SNAKE:
+                  PLAYER_POSITION-=rollDice;
+                  System.out.println("Snake " + PLAYER_POSITION);
+                  break;
+       default:
+                  System.out.println("No Play" + PLAYER_POSITION);
+      
+   }
+}
