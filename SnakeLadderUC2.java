db83254952601c76db581fc846c69c1ebe866939
batista12 <50904472+batista12@users.noreply.github.com>
2020-09-25 16:10:26 -0700

    Create SnakeLadderUC2.java

---
+++ SnakeLadderUC2.java
@@ -0,0 +1,12 @@
+public class SnakeLadderUC2 {
+   public static void main(String[] args){
+      
+      double rollDice =(int)(Math.random() * 6) +1;
+      System.out.println("Dice roll value : " + rollDice);
+   }
+}
+
+
+
+
+
