439d74bf9569b3d1442b5205ac1c5290f92cf0eb
batista12 <50904472+batista12@users.noreply.github.com>
2020-09-25 15:50:42 -0700

    Create SnakeLadderUC1.java

---
+++ SnakeLadderUC1.java
@@ -0,0 +1,10 @@
+                                                                                  EmpWageBuilderUC1.java
+public class SnakeUC1 {
+   public static void main(String[] args){
+      int INITIAL_POSITION = 0;
+         System.out.println("Initial position of player " + INITIAL_POSITION);
+   }
+}
+
+
+
