816283dad6895900b1bf083ead5b79e595d23bfe
batista12 <50904472+batista12@users.noreply.github.com>
2020-09-25 15:53:32 -0700

    Update SnakeLadderUC1.java

--- SnakeLadderUC1.java
+++ SnakeLadderUC1.java
@@ -1,5 +1,5 @@
                                                                                   EmpWageBuilderUC1.java
-public class SnakeUC1 {
+public class SnakeLadderUC1 {
    public static void main(String[] args){
       int INITIAL_POSITION = 0;
          System.out.println("Initial position of player " + INITIAL_POSITION);
