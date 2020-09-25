103832fdb89ff8d5a5a88cb01aeaa5aeb27c1859
batista12 <50904472+batista12@users.noreply.github.com>
2020-09-25 15:56:33 -0700

    Update SnakeLadderUC1.java

--- SnakeLadderUC1.java
+++ SnakeLadderUC1.java
@@ -2,7 +2,7 @@
 public class SnakeLadderUC1 {
    public static void main(String[] args){
       int INITIAL_POSITION = 0;
-         System.out.println("Initial position of player " + INITIAL_POSITION);
+         System.out.println("Initial position of player 1 is : " + INITIAL_POSITION);
    }
 }
 
