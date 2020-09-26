633aedbb513beba567b94931a481171d0f35cfcd
batista12 <50904472+batista12@users.noreply.github.com>
2020-09-25 18:40:11 -0700

    Create SnakeLadderUC6.java

---
+++ SnakeLadderUC6.java
@@ -0,0 +1,34 @@
+public class SnakeLadderUC5 {
+   public static void main(String[] args){
+      int PLAYER_POSITION=0,option=0,Count=0,NoOfTimes=0;
+      while(PLAYER_POSITION<100){
+          Count++;
+      
+      do{
+      int rollDice =(int)(Math.random() * 6) +1;
+      System.out.println("Dice Roll " + rollDice);
+      option =(int)(Math.random() * 3);
+      switch(option){
+      case 1:
+                  PLAYER_POSITION+=rollDice;
+                  System.out.println("Ladder");
+                  break;
+      case 2:
+                  PLAYER_POSITION-=rollDice;
+                  System.out.println("Snake");
+                  break;
+       default:
+                  System.out.println("No Play");
+
+   }NoOfTimes++;
+   if(PLAYER_POSITION<0)
+   PLAYER_POSITION=0;
+   if(PLAYER_POSITION>100)
+   PLAYER_POSITION=rollDice;
+   System.out.println("Current position : " + PLAYER_POSITION);
+   
+   }while(PLAYER_POSITION !=100 && option==1);
+   
+   }
+}System.out.println("Number of chances required to win " + NoOfTimes);
+}
